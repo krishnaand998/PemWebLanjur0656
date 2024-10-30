18d471b1185a56eeff5aaf071c1ff6080b88a474
krishnaand998 <123559043+krishnaand998@users.noreply.github.com>
2024-10-30 14:26:54 +0700

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from routes.Category_bp import category_bp
 app.register_blueprint(book_bp)
 app.register_blueprint(category_bp)
 
+db.create_all()
 # if __name__ == '__main__':
 #     db.create_all()
 #     app.run(debug=True)
