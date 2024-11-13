f491c4203cb3551cece050bc00b18fab8b523237
krishnaand998 <123559043+krishnaand998@users.noreply.github.com>
2024-11-13 15:49:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Configuring MySQL database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://krisdatabase_poetrythee:a3b75e70bf28a3654c989ddb682c83a9aa34b19e@5kyh4.h.filess.io:3307/krisdatabase_poetrythee'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://krisdatabase_poetrythee:a3b75e70bf28a3654c989ddb682c83a9aa34b19e@5kyh4.h.filess.io:3307/krisdatabase_poetrythee'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
